95ebef019eb0d3dcecc7a7eb0256c7326f984e0a
Reza Alipour <kondori.reza1990@gmail.com>
2021-12-26 22:54:23 +0330

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,26 +17,121 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int count = 0;
   Color color = Colors.red;
+  String text = "";
+
+  final FocusNode _focusNode = FocusNode();
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode1 = FocusNode();
+  final TextEditingController _controller1 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("First App"),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              FlutterLogo(size: 35,),
+              Text("Title Demo With Row",
+              style: TextStyle(fontFamily: "fontName"),)
+            ],
+          )
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Body $count",
-              style: TextStyle(color: color, fontSize: 18),
-            ),
-            const Text("My Flutter Project..."),
-            const Text("Hello World"),
-            const FlutterLogo(size: 300,)
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                style: const TextStyle(fontFamily: "fontName"),
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name',
+                  hintText: 'Please Enter name'
+                ),
+                focusNode: _focusNode,
+                autofocus: true,
+                controller: _controller,
+                enabled: true,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]')),
+                ],
+                keyboardType: TextInputType.text,
+                minLines: 1,
+                maxLength: 5,
+                obscureText: false,
+                obscuringCharacter: "*",
+                onChanged: (value) {
+                  setState(() {
+                    text = value;
+                  });
+                },
+                onSubmitted: (value) {
+                  _focusNode1.nextFocus();
+                },
+                onEditingComplete: () {
+
+                },
+                onTap: () {
+
+                },
+                readOnly: false,
+                textAlign: TextAlign.start,
+                textDirection: TextDirection.ltr,
+              ),
+              TextField(
+                style: const TextStyle(fontFamily: "fontName"),
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Family',
+                    hintText: 'Please Enter family'
+                ),
+                focusNode: _focusNode1,
+                autofocus: true,
+                controller: _controller1,
+                enabled: true,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]')),
+                ],
+                keyboardType: TextInputType.text,
+                minLines: 1,
+                maxLength: 20,
+                obscureText: false,
+                obscuringCharacter: "*",
+                onChanged: (value) {
+                  setState(() {
+                    text = value;
+                  });
+                },
+                onSubmitted: (value) {
+                  setState(() {
+                    text = _controller.text + " " + _controller1.text;
+                  });
+                },
+                onEditingComplete: () {
+
+                },
+                onTap: () {
+
+                },
+                readOnly: false,
+                textAlign: TextAlign.start,
+                textDirection: TextDirection.ltr,
+              ),
+              Text(
+                "Body $count, $text",
+                style: TextStyle(
+                    color: color,
+                    fontSize: 25,
+                  fontFamily: "fontName"
+                ),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
